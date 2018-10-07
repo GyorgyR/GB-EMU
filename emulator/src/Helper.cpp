809686ccbb0cf65345fb0c0117e12f3d4fc5e022
Gyorgy Rethy <rethygy0410@gmail.com>
2018-10-07 17:55:44 +0100

    Fix 8bit to 16bit operations.
    Signed-off-by: Gyorgy Rethy <rethygy0410@gmail.com>

--- emulator/src/Helper.cpp
+++ emulator/src/Helper.cpp
@@ -19,7 +19,7 @@ uint16_t Helper::ConcatTwoBytes(uint8_t first, uint8_t second) {
 
 std::pair<uint8_t, uint8_t> Helper::DivideIntoTwoBytes(uint16_t value) {
     const uint16_t bit_mask = 0b0000000011111111;
-    return std::make_pair(value & bit_mask, (value >> 8) & bit_mask);
+    return std::make_pair((value >> 8) & bit_mask, value & bit_mask);
 }
 
 Helper::Helper()
